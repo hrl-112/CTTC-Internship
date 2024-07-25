9fce1b8d1763a74f9b4b96ace0e935ee752a28d8
hrl-112 <hectorrl2003@gmail.com>
2024-07-25 12:59:04 +0200

    Update Install_drivers_JTAG_cable.sh

--- Scripts/Install_drivers_JTAG_cable.sh
+++ Scripts/Install_drivers_JTAG_cable.sh
@@ -1,1 +1,6 @@
-
+# How to install the drivers for using the JTAG cable.
+cd ~/Xilinx/Vitis/2021.2/data/xicom/cable_drivers/lin64/install_script/install_drivers
+sudo ./setup_xilinx_ftdi 
+sudo ./install_digilent.sh
+sudo ./install_drivers
+sudo ./setup_pcusb
