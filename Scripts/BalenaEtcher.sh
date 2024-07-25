11e58d8542ce678c8119fc8ae500368c0d079d49
hrl-112 <hectorrl2003@gmail.com>
2024-07-25 12:33:46 +0200

    Create BalenaEtcher.sh

---
+++ Scripts/BalenaEtcher.sh
@@ -0,0 +1,3 @@
+# How to open BalenaEtcher from the terminal
+## Once you have BalenaEtcher installed execute the following command to open it from terminal
+~/Descargas/balenaEtcher-linux-x64-1.19.21/balenaEtcher-linux-x64/balena-etcher &
