52e66c8572929a5a968b0d334aac6f810a6033cf
hrl-112 <hectorrl2003@gmail.com>
2024-08-06 12:59:43 +0200

    Update BalenaEtcher.sh

--- Scripts/BalenaEtcher.sh
+++ Scripts/BalenaEtcher.sh
@@ -1,3 +1,21 @@
-# How to open BalenaEtcher from the terminal
-## Once you have BalenaEtcher installed execute the following command to open it from terminal
-~/Descargas/balenaEtcher-linux-x64-1.19.21/balenaEtcher-linux-x64/balena-etcher &
+# How to download and open BalenaEtcher from the terminal
+# We go to Downloads and we install BalenaEtcher
+~Downloads
+wget https://github.com/balena-io/etcher/releases/download/v1.19.21/balenaEtcher-linux-x64-1.19.21.zip
+unzip balenaEtcher-linux-x64-1.19.21.zip
+
+# Then we create a script to launch BalenaEtcher
+echo '~/Downloads/balenaEtcher-linux-x64-1.19.21/balenaEtcher-linux-x64/balena-etcher &' > ~/balena.sh
+# We make it an executable 
+chmod +x ~/balena.sh
+
+# Add home folder to PATH
+export PATH=$PATH:~/
+
+# Create a simbolic link
+ln -s ~/balena.sh balena
+
+# To open BalenaEtcher from terminal:
+balena
+
+# IMPORTANT: From now on it is only required to write "balena" on the terminal in order to open BalenaEtcher
