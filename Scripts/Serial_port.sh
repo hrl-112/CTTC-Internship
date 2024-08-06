001c4ea9aa2049238231e8dfb02691bdef2ed7a1
hrl-112 <hectorrl2003@gmail.com>
2024-08-06 13:20:28 +0200

    Update Serial_port.sh

--- Scripts/Serial_port.sh
+++ Scripts/Serial_port.sh
@@ -1,3 +1,7 @@
 # How to open a serial port from terminal.
-sudo dmesg
-sudo gtkterm -p /dev/ttyUSB1 -s 115200 &
+sudo su
+gtkterm -p /dev/ttyUSB1 -s 115200 &
+
+# Here is a tip for using the gtktrem when debbuging:
+# Once the gtkterm is open before doing anything, resize the window. 
+# For short messages there is no problem but when this is not the case the gtkterm gets bugged and you can barely see what you are writing, personal experience.
